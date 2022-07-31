284c3385ec7f26b41a55629cc3b821d45c5ef972
Masih-Saldanha <masih.saldanha@gmail.com>
2022-07-31 14:28:59 -0300

    feat: add downVoteRecommendation e2e test

---
+++ front-end/cypress/e2e/tests/downVoteRecommendation.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+import recommendationFactory from "../../../factories/recommendationFactory.js";
+
+describe("Home Screen", () => {
+    it("Should upvote an recommendation", async () => {
+        cy.seedDB();
+
+        cy.intercept("GET", `${recommendationFactory.URL_BACK}`).as("getRecommendations");
+        cy.visit(recommendationFactory.URL_FRONT);
+        cy.wait("@getRecommendations");
+
+        cy.intercept("POST", `${recommendationFactory.URL_BACK}/1/downvote`).as("downVote");
+        cy.get("article div svg").eq(1).click();
+        cy.wait("@downVote");
+
+        cy.get("article div").should("contain.text", -1);
+    });
+});
